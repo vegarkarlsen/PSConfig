2efbbb72103eac2006fbda00824a1179bb0538b6
Hurodor <vegkar@stud.ntnu.no>
2022-09-20 18:37:50 +0200

    made aliases for explorer and qucik edit on alias file

--- Aliases.ps1
+++ Aliases.ps1
@@ -1,5 +1,5 @@
 # Custom aliases
-Set-Alias ntb C:\Users\$env:USERNAM\Documents\WindowsPowerShell\Scripts\ntnu_auto.ps1 -Scope global
+Set-Alias ntb C:\Users\$env:USERNAME\Documents\WindowsPowerShell\Scripts\ntnu_auto.ps1 -Scope global
 Set-Alias touch new-item -Scope global
 Set-Alias activate .\venv\Scripts\activate.ps1 -Scope global
 Set-Alias which where.exe -Scope global
@@ -7,9 +7,14 @@ Set-Alias sudo gsudo -Scope global
 Set-Alias grep Select-String -Scope global
 Set-Alias zip Compress-Archive -Scope global
 set-alias unzip Expand-Archive -Scope global
+Set-Alias e explorer.exe -Scope global
 
+# Quick accsess to this file
+function global:malias {nano "C:\Users\vegar\Documents\WindowsPowerShell\PSConfig\Aliases.ps1"}
 
 
 function global:l { Get-ChildItem $args -Exclude .* -n}
 function global:ll { Get-ChildItem $args -n -Hidden}  
+
 function global:home {Set-Location "C:\Users\$env:USERNAME\"} 
+
